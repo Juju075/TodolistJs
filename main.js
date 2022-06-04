37c36093a41187a6018f007d674fcb1f67c4d974
elgato <amzfba.1bestbuy@gmail.com>
2022-06-04 22:20:00 +0200

    main:

--- main.js
+++ main.js
@@ -21,6 +21,10 @@ window.addEventListener("load", () => {
       alert("Please fill out the task!!!");
       return; // met fin à l'execution
     } else {
+      // =============================================================================
+      //
+      // =============================================================================
+
       // CreateElement   'function d'ajout de tache dans quoi?' 'ajout de bloc html'
 
       // [1] Create | <div class="task"></div>
@@ -29,13 +33,14 @@ window.addEventListener("load", () => {
       /**debug */
       console.log(taskEl);
 
-      // [2] Create | <div class="content"> TaskInput content in
+      // [2] Create | <div class="content"> TaskInput </content>
       const taskContentEl = document.createElement("div");
       taskContentEl.classList.add("content");
       taskContentEl.innerHTML = taskInput;
       /**debug */
       console.log(taskContentEl);
 
+      // Ajout
       // [3] Injection | <div class="task"> ajout content // ok fonctionne
       taskEl.appendChild(taskContentEl);
       /**debug */
@@ -54,25 +59,47 @@ window.addEventListener("load", () => {
       taskContentEl.appendChild(taskInputEl);
       console.log(taskContentEl.appendChild(taskInputEl));
 
-      // [6] Create |
-      const taskContentEl1 = document.createElement("div");
-      taskContentEl1.classList.add("actions");
+      // =============================================================================
+      // New Elements
+      // =============================================================================
 
-      // [7] Create |
+      // [6] Create | <div class="actions"></div>
+      const taskActionEl = document.createElement("div");
+      taskActionEl.classList.add("actions");
+
+      // [7] Create | <button class="edit"></button>
       const taskEditEl = document.createElement("button");
       taskEditEl.classList.add("edit"); //action
+      console.log(taskEditEl);
 
-      // [8] Create | ok
+      // [8] Create | <button class="delete">delete</button> | ok
       const taskDeleteEl = document.createElement("button");
-      taskDeleteEl.classList.add("delete"); //action
+      taskDeleteEl.classList.add("delete"); // class="delete"
       taskDeleteEl.innerHTML = "Delete"; // <element> Delete </element>
       /**debug */
       console.log(taskDeleteEl);
 
+      // =============================================================================
+      //  Injections HTML
+      // =============================================================================
+
+      // Ajouter node child dans le button "edit"
       // [9] Injection |Espected: <div class="tasks"><div class="content">Saisie user</div></div> ok
+      taskActionEl.appendChild(taskEditEl);
+
+      // Ajouter node child dans le button "delete"
+      taskActionEl.appendChild(taskDeleteEl);
+
+      // Ajouter node child dans la div "actions"
+      taskEl.appendChild(taskActionEl);
+
+      // Ajouter node child dans <div class="task"></div>
       listEl.appendChild(taskEl);
+
       /**debug */
       console.log(listEl.appendChild(taskEl));
+
+      input.value = "";
     }
   });
 });
